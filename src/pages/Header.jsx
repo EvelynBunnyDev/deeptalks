8eb17e7964a5a92beab47052c3818cec84a0bfc6
EvelynBunnyDev <evsong@stanford.edu>
2024-05-14 01:57:25 -0700

    updates

---
+++ src/pages/Header.jsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { ButtonBase, Grid, Popover, Menu, MenuItem, Link } from "@mui/material/";
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+
+let barStyle = {
+  color: "black",
+  backgroundColor: "white",
+  padding: "10px 35px",
+  borderBottom: "3px solid rgba(0, 0, 0, .09)"
+};
+
+let buttonStyle = {
+  padding: "5px 20px",
+  color: "black",
+  // marginLeft: "auto"
+};
+
+export default function NavBar(props) {
+  const {title, post, type} = props;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+
+  let createPostButton
+  if (post === "true") {
+    if (type === "home") {
+      createPostButton = <a href="/createPost" style={buttonStyle}><Button>Create a post</Button></a>;
+    } else if (type === "group") {
+      createPostButton = <a href="/createGroupPost" style={buttonStyle}><Button>Create a post</Button></a>;
+    }
+  }
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" elevation={0}>
+          <Toolbar style={barStyle}>
+            <Typography variant="h5" style={{ marginLeft: "25px", color: "#33363F", whiteSpace: "nowrap" }}>
+              <b>{title}</b>
+            </Typography>
+            <Grid container columnSpacing={2} justifyContent="flex-end" alignItems="center">
+              <Grid item>
+                {createPostButton}
+                <Link href="/Inbox" style={{ textDecoration: 'none' }}>
+                <Button style={buttonStyle}>View Inbox</Button>
+                </Link>
+                <Link href="/Journal" style={{ textDecoration: 'none' }}>
+                <Button style={buttonStyle}>View Journal Entries</Button>
+                </Link>
+              </Grid>
+              <Grid item>
+                <ButtonBase variant="contained" onClick={handleClick}>
+                  <AccountCircleRoundedIcon fontSize="large" sx={{color:"#33363F"}}/> <ExpandMoreRoundedIcon ontSize="large" sx={{color:"#33363F"}}/>
+                </ButtonBase>
+                <Menu
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                >
+                  
+                  <a href="/Profile"><MenuItem sx={{color: "#33363F"}}>Account Settings</MenuItem></a> 
+                  <MenuItem sx={{color: "darkred"}}>Logout</MenuItem>
+                </Menu>
+              </Grid>
+
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,74 +1,89 @@
-// Import necessary components from Material-UI and React Router
-import { Container, Typography, Button, Box, AppBar, Tab, Tabs } from '@mui/material';
+// Import necessary components from Material-UI, React Router, and local components
+import React, { useState } from 'react';
+import { Container, Typography, Button, Box, AppBar, Tab, Tabs, Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import NavBar from './Header';
+import Threads from './Threads'; // Assuming Thread.jsx exports a component that lists threads
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          {children}
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
 }
 
 export default function Home() {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = useState(0);
+    const [threads, setThreads] = useState([
+      { 
+          id: 1, 
+          title: "Life After College", 
+          content: "Discuss your experiences and future plans post-graduation." 
+      },
+      { 
+          id: 2, 
+          title: "Managing Stress", 
+          content: "Share tips and strategies for managing daily stress." 
+      }
+  ]);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Container maxWidth="lg">
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Threads" />
-          <Tab label="Inbox" />
-          <Tab label="My Journal" />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <Typography variant="h4">Welcome to DeepTalks!</Typography>
-        <Button variant="contained" color="primary" component={Link} to="/signup">
-          Sign up with stanford.edu email
-        </Button>
-        <Button variant="contained" component={Link} to="/create-thread" sx={{ mt: 2 }}>
-          Throw a Thought
-        </Button>
-        <Button variant="contained" component={Link} to="/commenting" sx={{ mt: 2 }}>
-          Comment on a Thought
-        </Button>
-        <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
-          <Typography variant="h6">Recent Threads</Typography>
-          {/* Dynamic list rendering based on state or props */}
-        </Box>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
+    return (
+        <Container maxWidth="lg">
+            <NavBar />
+            
+            <TabPanel value={value} index={0}>
+                <Typography variant="h4">Welcome to DeepTalks!</Typography>
+                <Button variant="contained" color="primary" component={Link} to="/signup">
+                    Sign up with stanford.edu email
+                </Button>
+                <Button variant="contained" component={Link} to="/create-thread" sx={{ mt: 2 }}>
+                    Throw a Thought
+                </Button>
+                <Button variant="contained" component={Link} to="/commenting" sx={{ mt: 2 }}>
+                    Comment on a Thought
+                </Button>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <Threads /> {/* Render the Thread component */}
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                {/* Other content if needed */}
+                <Typography variant="h6">Additional Resources or Content</Typography>
+            </TabPanel>
 
-       <Typography variant="h6">Inbox</Typography>
-        {/* Adding a Link component to navigate to Inbox */}
-        <Link to="/inbox" style={{ textDecoration: 'none' }}>
-        <Button variant="contained" color="primary">
-            Go to Inbox
-        </Button>
-    </Link>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Typography variant="h6">My Journal</Typography>
-        {/* Dynamic journal entries rendering */}
-      </TabPanel>
-    </Container>
-  );
+            <Box>
+                    { threads.length === 0 && <Typography variant="body1">No threads available.</Typography>}
+                    {
+                        threads.map((thread) => (
+                            <Paper key={thread.id} elevation={3} sx={{ padding: 2, marginBottom: 2 }}>
+                                <Typography variant="h6">{thread.title}</Typography>
+                                <Typography>{thread.content}</Typography>
+                                {true && (
+                                    <Button component={Link} to={`/thread/${thread.id}`} sx={{ mt: 1 }}>
+                                        Comment
+                                    </Button>
+                                )}
+                            </Paper>
+                        ))
+                    }
+                </Box>
+        </Container>
+    );
 }
